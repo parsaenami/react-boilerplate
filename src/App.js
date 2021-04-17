2d45aa9a8b14aada04ed8acfa34e6d81c8b09d3d
Parsa Enami <p.enaami@digikala.com>
2021-04-17 22:12:40 +0430

    created boilerplate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import HttpsRedirect from 'react-https-redirect';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Helmet from 'react-helmet';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <HttpsRedirect>
+        <BrowserRouter>
+          <Helmet defaultTitle="App" titleTemplate="App | %s"/>
+          <div className="App">
+            What a nice ReactJS boilerplate by Parsa!
+          </div>
+        </BrowserRouter>
+      </HttpsRedirect>
   );
 }
 
